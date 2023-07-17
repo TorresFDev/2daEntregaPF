62cbee38f57888cddfcb6d044220d78c7df1cb49
FacundoTorres <torresfdev@gmail.com>
2023-07-17 13:25:51 -0300

    se armo la estructura del proyecto y se empezo con los productos

---
+++ src/dao/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import productRouter from "./routers/products.router.js";
+import mongoose, { mongo } from "mongoose";
+import handlebars from "express-handlebars";
+import viewRouter from "./routers/view.router.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+app.use(express.static("./public"));
+
+app.use("/api/products", productRouter);
+
+app.use("/api/cart", cartRouter);
+
+app.use("/products", viewRouter);
+
+await mongoose.connect(
+  "mongodb+srv://coder:coder@cluster0.i2hgwbg.mongodb.net/ecommmerce"
+);
+
+const httpServer = app.listen(8080, () => {
+  console.log("estoy en ejecucion");
+});
